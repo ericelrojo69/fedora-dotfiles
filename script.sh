3cc91117c3d9e55bbd378a1258f4da89e7daca77
Eric <55553858+ericelrojo69@users.noreply.github.com>
2023-02-28 16:12:42 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo dnf install python3-cffi python3-cairocffi pango-devel python3-dbus-next python3-pip wlroots-devel -y
-pip install pywlroots pywayland python-xkbcommon
+pip install pywlroots pywayland xkbcommon
